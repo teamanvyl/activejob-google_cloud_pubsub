1f577b4144f0c24b8cd6e4e9443468edffdb8399
Jason Jho <jason@anvyl.com>
2017-10-17 13:52:21 -0400

    Pin to pubsub client library versio 0.26.0 to preserve listen API semantics

--- lib/activejob-google_cloud_pubsub/pubsub_extension.rb
+++ lib/activejob-google_cloud_pubsub/pubsub_extension.rb
@@ -6,13 +6,11 @@ module ActiveJob
       refine Google::Cloud::Pubsub::Project do
         def topic_for(queue_name)
           name = "activejob-queue-#{queue_name}"
-
-          topic(name, autocreate: true)
+          topic(name) || create_topic(name)
         end
 
         def subscription_for(queue_name)
           name = "activejob-worker-#{queue_name}"
-
           subscription(name) || topic_for(queue_name).subscribe(name)
         end
       end
